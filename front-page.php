e9c4b4400f3983680cc4e90e614bba18df1721e9
Tfuqua <tfuqua@gwu.edu>
2016-10-13 00:09:54 -0400

    Prep for Mrs.Marshall's for demo

--- front-page.php
+++ front-page.php
@@ -10,11 +10,11 @@ get_header(); ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
 
+
     <!-- Hero Image -->
     <?php if(get_field('hero_image')) { ?>
-    	<div class="hero">
+      <div class="hero hero-1">
         <?php echo wp_get_attachment_image(get_field('hero_image'), 'full', false, array( 'class' => '') ); ?>
-
         <div class="hero-text">
           <?php echo get_field('hero_text')?>
           <?php if(get_field('button_text')) { ?>
@@ -29,26 +29,33 @@ get_header(); ?>
     <!-- Featured Content -->
     <?php
       if( have_rows('featured_content') ) { ?>
-        <div class="featured-content">
+        <div class="featured-content-wrapper">
+          <div class="container-fluid">
             <?php while ( have_rows('featured_content') ) : the_row(); ?>
                 <div class="featured-item">
                   <h3><?php echo the_sub_field('heading');?></h3>
 
                   <div class="featured-section">
                     <div class="featured-body">
-                      <?php echo the_sub_field('body');?>
+                      <div class="flex-div">
+                      <div>
+                        <?php echo the_sub_field('body');?>
+                      </div>
+                      <div class="featured-img">
+                        <?php echo wp_get_attachment_image(get_sub_field('image'), 'medium', false, array( 'class' => 'lazy-load'));?>
+                      </div>
+                      </div>
                       <div class="featured-button">
                         <a>
                           <?php echo the_sub_field('button_text');?>
                         </a>
                       </div>
-                    </div>
-                    <div class="featured-img">
-                      <?php echo wp_get_attachment_image(get_sub_field('image'), 'medium', false, array( 'class' => 'lazy-load'));?>
-                    </div>
+
+                    </DIV>
                   </div>
                 </div>
             <?php endwhile; ?>
+          </div>
         </div>
       <?php } ?>
 
