bb6be9204fa16f248b508f3e0d10b12be4365c96
Taylor Fuqua <tfuqua@gwu.edu>
2016-10-20 14:46:09 -0400

    Miss Marshall style fixes
    #1, #2, #3, #4, #5, #6, #7, #8

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ function mrsmarshalls_enqueue_styles() {
     $parent_style = 'parent-style'; // This is for general Ukrops theme styling
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
+
+    wp_enqueue_style( 'google-font-kalam', 'https://fonts.googleapis.com/css?family=Kalam', false);
   	wp_enqueue_style( 'ukrops-style', get_stylesheet_directory_uri() . '/style.min.css');
 }
 
